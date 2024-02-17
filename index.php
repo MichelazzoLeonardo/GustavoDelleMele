c5b7ec4fedc3eec803547205bae054c888bc1a13
Leonardo <8293569@itisrossi.vi.it>
2024-02-17 11:39:30 +0100

    creazione database automatizzata

--- index.php
+++ index.php
@@ -1,4 +1,41 @@
 <?php
+//creating database
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'minecraft_farms';
+
+$conn = new mysqli($servername, $username, $password);
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname;";
+
+$conn->query($sql);
+
+//creating tables
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "CREATE TABLE IF NOT EXISTS user (
+  username varchar(64) NOT NULL,
+  email varchar(64) NOT NULL,
+  password varchar(60) NOT NULL
+);";
+$conn->query($sql);
+
+$sql = "CREATE TABLE IF NOT EXISTS farm (
+  owner varchar(64) NOT NULL,
+  name varchar(64) NOT NULL PRIMARY KEY,
+  version varchar(64) NOT NULL,
+  rates varchar(500) NOT NULL,
+  type varchar(16) NOT NULL,
+  afkable varchar(16) NOT NULL,
+  overworld varchar(16) NOT NULL,
+  nether varchar(16) NOT NULL,
+  end varchar(16) NOT NULL,
+  tutorial varchar(200) NOT NULL,
+
+  FOREIGN KEY (owner) REFERENCES user(username)
+);";
+$conn->query($sql);
+
 header('Location:pages/community.php');
 
 /*
